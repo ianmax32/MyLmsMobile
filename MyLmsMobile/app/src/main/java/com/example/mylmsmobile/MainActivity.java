fe182c20d2bde96e62a456580112e7fd132222f1
ianmax32 <masagaian32@gmail.com>
2023-06-08 13:03:59 +0200

    Login implemented

--- MyLmsMobile/app/src/main/java/com/example/mylmsmobile/MainActivity.java
+++ MyLmsMobile/app/src/main/java/com/example/mylmsmobile/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.mylmsmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
     }
 }
